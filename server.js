04eab1b0c0e323197a6237b0377e62f2bc0e66e1
suwu2004 <suwu4467@gmail.com>
2026-06-18 10:01:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
-
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
